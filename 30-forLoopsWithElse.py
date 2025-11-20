d4633b6819a8dc02a88408d52aac941c00ca4283
Ashish <ashishsindhav02@outlook.com>
2025-11-20 21:12:21 +0530

    day 10: for loop with else done

---
+++ 30-forLoopsWithElse.py
@@ -0,0 +1,27 @@
+# Python allows the else keyword to be used with the for and while loops too. The else block appears after the body of the loop. The statements in the else block will be executed after all iterations are completed. The program exits the loop only after the else block is executed.
+
+for i in range(5):
+    if i == 4:
+        break
+    print(i)
+
+else:
+    print("Sorry no i")
+
+
+i = 0
+
+while i < 7:
+    print(i)
+    i = i + 1
+    if i == 4:
+        break
+else:
+    print("Sorry no i")
+
+
+for x in range(5):
+    print("iteration no {} in for loop.".format(x+1))
+else:
+    print("Else block in loop")
+print("Out of loop")
